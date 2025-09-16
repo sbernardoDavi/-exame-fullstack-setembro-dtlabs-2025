86e0c03a7f866896a2860839c236589cb64795b3
Davi Bernardo <sbernardo.davi@outlook.com>
2025-09-16 14:36:34 -0300

    feat: roteamento de paginas

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,23 @@
+// src/App.tsx
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import LoginPage from '../Login/loginPage';
-
+import HomePage from '../Home/homePage';
+import PrivateRoute from '../PrivateRoute/privateRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <LoginPage />
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={
+          <PrivateRoute>
+            <HomePage />
+          </PrivateRoute>
+        } />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
   );
 }
 

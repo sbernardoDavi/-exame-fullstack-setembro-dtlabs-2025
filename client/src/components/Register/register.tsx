6844e3d1b3a4554987d8ccbcfc5f1ec8813f62e3
Davi Bernardo <sbernardo.davi@outlook.com>
2025-09-19 00:23:20 -0300

    feat: pagina para registrar usuario.

---
+++ client/src/components/Register/register.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { register } from '../../services/registerUser';
+import { FaUserPlus } from "react-icons/fa6";
+import './register.css';
+
+function RegisterPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+
+  const handleRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await register(email, password);
+      setMessage('Usuário cadastrado com sucesso!');
+      setTimeout(() => navigate('/login'), 4500);
+    } catch (error: any) {
+      setMessage(error.message || 'Erro ao cadastrar usuário.');
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <h2> <FaUserPlus /> Cadastro</h2>
+      <form onSubmit={handleRegister} className="register-form">
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Cadastrar</button>
+
+        {message && <p className="register-message">{message}</p>}
+      </form>
+    </div>
+  );
+}
+
+export default RegisterPage;
---
+++ client/src/services/registerUser.tsx
@@ -0,0 +1,15 @@
+export async function register(email: string, password: string): Promise<void> {
+  const response = await fetch('http://localhost:5000/api/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.error || 'Erro ao cadastrar');
+  }
+}
